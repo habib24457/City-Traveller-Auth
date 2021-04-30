96e3e04ef638d8a21585e5c6d6c2d88906c7a684
Habibur Rahman <rahmanhabibur24457@gmail.com>
2021-04-30 02:49:13 +0200

    added some extra feature on result page

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -1,110 +1,187 @@
-import React, { useContext,useState,useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import ridesData from '../../Data/Data.json';
-import {UserContext} from "../../App";
+import { UserContext } from "../../App";
 import './Result.css';
-import {Navbar,Nav,Container,Form,Button} from 'react-bootstrap';
-import {useParams} from 'react-router-dom';
+import { Navbar, Nav, Container, Form, Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 //import {Link} from 'react-router-dom';
 //import {GoogleMapReact,Map,Marker} from 'google-map-react';
 
-import mapImg from '../../images/Map.png';
-
 
 const Result = () => {
-    let {rideId} = useParams();
-    const [ride,setRide] = useState({});
-    const [searchResult,setSearchResult] = useState({
+    let { rideId } = useParams();
+    const [ride, setRide] = useState({});
+
+    const [youngTicket,setYoungTicket] = useState("0");
+    const [youngTPrice,setYoungTPrice] = useState("0");
+
+    const [adultTicket,setAdultTicket] = useState("0");
+    const [adultTPrice,setAdultTPrice] = useState("0");
+
+
+    const [searchResult, setSearchResult] = useState({
         success: false,
-        locationFrom:'',
-        locationTo:'',
+        locationFrom: '',
+        locationTo: '',
     })
 
     useEffect(() => {
-        const data = ridesData.filter(s => s.id == rideId);
+        const data = ridesData.filter(s => s.id === rideId);
         setRide(data);
-    },[rideId])
+    }, [rideId])
     //console.log("Useparams",ride[0]?.img);
 
     const carImg = ride[0]?.img;
-    console.log("car=",carImg);
+    console.log("car=", carImg);
 
-    const [loggedinUser,setLoggedinUser] = useContext(UserContext);
+    const [loggedinUser, setLoggedinUser] = useContext(UserContext);
     console.log(setLoggedinUser);
 
-    const handleSearch=()=>{
-        const newSearchResult = {...searchResult};
+    const handleSearch = () => {
+        const newSearchResult = { ...searchResult };
         newSearchResult.success = true;
         setSearchResult(newSearchResult);
     }
 
-    const handleBlur = (e)=>{
-        const newSearchResult = {...searchResult};
-        newSearchResult[e.target.name]= e.target.value;
+    const handleBlur = (e) => {
+        const newSearchResult = { ...searchResult };
+        newSearchResult[e.target.name] = e.target.value;
         setSearchResult(newSearchResult);
     }
 
+    const calculateTotal =(ticketPrice)=>{
+       console.log(youngTPrice);
+       console.log(adultTPrice);
+    }
 
+    const handleYoungTicket=(e)=>{
+        e.preventDefault();
+       // console.log("Zara",e.target.getAttribute('value'));
+
+        if(e.target.getAttribute('name')=== 'youngPlus'){ 
+                      
+            let ticketAmount = document.getElementById("youngTicketAmount").value;
+
+            let ticketAmountNumber = parseInt(ticketAmount);            
+          
+           let finalTicketAmount = ticketAmountNumber+1;
+           let ticketPrice = 100*finalTicketAmount;
+           setYoungTPrice(ticketPrice);
+           setYoungTicket(finalTicketAmount);
+           calculateTotal(ticketPrice); 
+           
+          
+        }
+
+        if(e.target.getAttribute('name')=== 'youngMinus'){            
+            let ticketAmount = document.getElementById("youngTicketAmount").value;
+
+            let ticketAmountNumber = parseInt(ticketAmount);           
+            let finalTicketAmount = ticketAmountNumber-1; 
+            let ticketPrice = 100*finalTicketAmount; 
+            setYoungTicket(finalTicketAmount);
+            setYoungTPrice(ticketPrice);
+            calculateTotal(ticketPrice); 
+        }
+
+
+        if(e.target.getAttribute('name')=== 'adultPlus'){            
+            let ticketAmount = document.getElementById("adultTicketAmount").value;
+            let ticketAmountNumber = parseInt(ticketAmount);          
+            let finalTicketAmount = ticketAmountNumber+1; 
+            let ticketPrice = 150*finalTicketAmount;
+            setAdultTicket(finalTicketAmount);
+            setAdultTPrice(ticketPrice);
+            calculateTotal(ticketPrice); 
+        }
+
+        if(e.target.getAttribute('name')=== 'adultMinus'){            
+            let ticketAmount = document.getElementById("adultTicketAmount").value;
+            let ticketAmountNumber = parseInt(ticketAmount);  
+            let finalTicketAmount = ticketAmountNumber-1; 
+            let ticketPrice = 150*finalTicketAmount; 
+            setAdultTicket(finalTicketAmount);  
+            setAdultTPrice(ticketPrice);
+            calculateTotal(ticketPrice); 
+        }
+         
+    }
+
+   
+   
     return (
         <Container>
-            <div className="row">  
-             <div className="col-md-12">     
-                <Navbar bg="light" variant="light">
-                <Navbar.Brand href="/home">CITY TRAVELLER</Navbar.Brand>
-                <Navbar.Collapse className="justify-content-end">
-                <Nav>
-                <Nav.Link href="/home">Home</Nav.Link>
-                <Nav.Link href="#">Destination</Nav.Link>
-                <Nav.Link style={{color:'green'}} href="#features">User:{loggedinUser.name}</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-                </Navbar>
-             </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <Navbar bg="light" variant="light">
+                        <Navbar.Brand href="/home">CITY TRAVELLER</Navbar.Brand>
+                        <Navbar.Collapse className="justify-content-end">
+                            <Nav>
+                                <Nav.Link href="/home">Home</Nav.Link>
+                                <Nav.Link href="#">Destination</Nav.Link>
+                                <Nav.Link style={{ color: 'green' }} href="#features">User:{loggedinUser.name}</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
             </div>
 
             <div className="row rowDesign">
                 <div className="col-md-4 ">
                     <Form className="search-design">
-                    <input onBlur={handleBlur}  className="form-control" type="text" 
-                     placeholder="Pick from" name="locationFrom" required/>         
-                    <br/>
-                    <input onBlur={handleBlur} className="form-control" type="text" 
-                     placeholder="Pick to" name="locationTo" required/>         
-                    <br/>
-                    <Button onClick={handleSearch} className="btn btn-warning">Search</Button>
-                    </Form>                    
+                        <input onBlur={handleBlur} className="form-control" type="text"
+                            placeholder="Pick from" name="locationFrom" required />
+                        <br />
+                        <input onBlur={handleBlur} className="form-control" type="text"
+                            placeholder="Pick to" name="locationTo" required />
+                        <br />
+                        <Button onClick={handleSearch} className="btn btn-warning">Search</Button>
+                    </Form>
 
                     {
-                      searchResult.success && 
-                      <div className="search-result">
-                        <h3>Search Result</h3>
-                        <div className="location-detail">
-                        <h4>From:{searchResult.locationFrom}</h4>
-                        <h4>To:{searchResult.locationTo}</h4>
-                        </div> 
+                        searchResult.success &&
+                        <div className="search-result">
+                            <h3>Search Result</h3>
+                            <div className="location-detail">
+                                <h4>From:{searchResult.locationFrom}</h4>
+                                <h4>To:{searchResult.locationTo}</h4>
+                            </div>
 
-                        <div> 
-                        <img src={carImg} alt="c"/>
-                        </div>                                                                    
-                     </div>
-                      
+                            <div>
+                                <img src={carImg} alt="c" />
+                            </div>
+                        </div>
+
                     }
 
                 </div>
 
-                <div className="col-md-8 show-map">
+                <div className="col-md-4 show-map">
+                    <Form className="search-design">
+                        <Form.Group>
+                            <Form.Label>Ticket for young people = 100$</Form.Label>
+                            <Form.Control id="youngTicketAmount" value={youngTicket} type="Text" required readOnly/>
+                            <h3 style={{cursor:'pointer'}}><span name="youngMinus" onClick={handleYoungTicket}>-</span> <span name="youngPlus" onClick={handleYoungTicket} >+</span></h3>
+                        </Form.Group>
 
-                   <img src= {mapImg} alt="map"/>
-                   
-                   {/* {
-                   google map properties (Map,Marker) weren't importing. Although dependenties r installed
-                    <h1>Show Map</h1>
-                    <Map google={this.props.google} zoom={14}>
-                    <Marker onClick={this.onMarkerClick}
-                     name={'Current location'} />
-                    </Map>
-                }  */}
-
+                        <Form.Group>
+                            <Form.Label>Ticket for adult = 150$</Form.Label>
+                            <Form.Control id="adultTicketAmount" value={adultTicket} type="Text" required readOnly/>
+                            <h3 style={{cursor:'pointer'}}><span name="adultMinus" onClick={handleYoungTicket}>-</span> <span name="adultPlus" onClick={handleYoungTicket}>+</span></h3>
+                        </Form.Group>
+                    </Form>
                 </div>
+
+                    <div className="col-md-4">
+                        <h3>You have purchased</h3>
+                        <p id="young-ticket">Sub Total Young ={youngTPrice} </p>
+                        <p>Sub Total Adult = {adultTPrice} </p>
+                        <p>Tax(15%) = </p>
+                        <hr/>
+                        <h3>Total = </h3>
+                        <Button variant="warning">Book Your Ride</Button>
+                    </div>
+
             </div>
         </Container>
     );
